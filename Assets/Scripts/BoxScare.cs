79ea2ce62521b46b3e3ac563538815ed5d7842d4
Jose-MiguelRz <jose.ruizmz@outlook.com>
2025-11-13 19:36:23 -0600

    Primer commit del proyecto Unity

---
+++ Assets/Scripts/BoxScare.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoxScare : MonoBehaviour
+{
+    public GameObject boxHolder;
+
+    void OnTriggerEnter(Collider other)
+    {
+        boxHolder.SetActive(false);
+    }
+
+
+}
